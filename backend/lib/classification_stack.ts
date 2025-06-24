f7b2e3db37a05ba08d4e809fdb026e0c4c38d086
Ridham <ridhamsonani3@gmail.com>
2025-06-24 12:19:32 -0700

    Updated build

--- backend/lib/classification_stack.ts
+++ backend/lib/classification_stack.ts
@@ -1,124 +1,338 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cdk from "aws-cdk-lib"
+import type { Construct } from "constructs"
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as s3 from "aws-cdk-lib/aws-s3"
+import * as iam from "aws-cdk-lib/aws-iam"
+import * as events from "aws-cdk-lib/aws-events"
+import * as targets from "aws-cdk-lib/aws-events-targets"
 
 export class ForestClassificationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // Define CloudFormation parameters without default values
-    const assetsBucketNameParam = new cdk.CfnParameter(this, 'AssetsBucketName', {
-      type: 'String',
-      description: 'The name of the S3 bucket containing Lambda layers and function code.',
-    });
+    const assetsBucketNameParam = new cdk.CfnParameter(this, "AssetsBucketName", {
+      type: "String",
+      description: "The name of the S3 bucket containing Lambda layers and function code.",
+    })
 
-    const bucketNameParam = new cdk.CfnParameter(this, 'BucketName', {
-      type: 'String',
-      description: 'The name of the S3 bucket to store input/output files. Must be globally unique.',
-    });
+    const bucketNameParam = new cdk.CfnParameter(this, "BucketName", {
+      type: "String",
+      description: "The name of the S3 bucket to store input/output files. Must be globally unique.",
+    })
 
-    const geeCredentialsFileParam = new cdk.CfnParameter(this, 'GeeCredentialsFile', {
-      type: 'String',
-      description: 'Name of the Google Earth Engine credentials file in the S3 bucket (e.g., credentials/custom-gee-credentials.json)',
-    });
+    const geeCredentialsFileParam = new cdk.CfnParameter(this, "GeeCredentialsFile", {
+      type: "String",
+      description:
+        "Name of the Google Earth Engine credentials file in the S3 bucket (e.g., credentials/custom-gee-credentials.json)",
+    })
 
     // Use parameter values
-    const assetsBucketName = assetsBucketNameParam.valueAsString;
-    const bucketName = bucketNameParam.valueAsString;
-    const geeCredentialsFile = geeCredentialsFileParam.valueAsString;
+    const assetsBucketName = assetsBucketNameParam.valueAsString
+    const bucketName = bucketNameParam.valueAsString
+    const geeCredentialsFile = geeCredentialsFileParam.valueAsString
 
     // Reference the assets bucket
-    const assetsBucket = s3.Bucket.fromBucketName(this, 'AssetsBucket', assetsBucketName);
+    const assetsBucket = s3.Bucket.fromBucketName(this, "AssetsBucket", assetsBucketName)
 
     // Create an S3 bucket for storing input/output files
-    const bucket = new s3.Bucket(this, 'ForestClassificationBucket', {
+    const bucket = new s3.Bucket(this, "ForestClassificationBucket", {
       bucketName: bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      cors: [{
-        allowedOrigins: ['*'],
-        allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT],
-        allowedHeaders: ['*'],
-        exposedHeaders: [],      // optional
-        maxAge: 3600             // optional
-      }]
-    });
+      cors: [
+        {
+          allowedOrigins: ["*"],
+          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT],
+          allowedHeaders: ["*"],
+          exposedHeaders: [], // optional
+          maxAge: 3600, // optional
+        },
+      ],
+    })
 
     // Create the Lambda function layers with fixed S3 keys
-    const earthEngineLayer = new lambda.LayerVersion(this, 'EarthEngineLayer', {
-      code: lambda.Code.fromBucket(assetsBucket, 'layers/earth_engine_layer.zip'),
+    const earthEngineLayer = new lambda.LayerVersion(this, "EarthEngineLayer", {
+      code: lambda.Code.fromBucket(assetsBucket, "layers/earth_engine_layer.zip"),
       compatibleRuntimes: [lambda.Runtime.PYTHON_3_13],
-      description: 'Earth Engine API and dependencies',
-    });
+      description: "Earth Engine API and dependencies",
+    })
 
-    const imageProcessingLayer = new lambda.LayerVersion(this, 'ImageProcessingLayer', {
-      code: lambda.Code.fromBucket(assetsBucket, 'layers/image_processing.zip'),
+    const imageProcessingLayer = new lambda.LayerVersion(this, "ImageProcessingLayer", {
+      code: lambda.Code.fromBucket(assetsBucket, "layers/image_processing.zip"),
       compatibleRuntimes: [lambda.Runtime.PYTHON_3_13],
-      description: 'PIL, Shapely, and other image processing libraries',
-    });
+      description: "PIL, Shapely, and other image processing libraries",
+    })
 
     // Create the Lambda function using code from S3 with fixed S3 key
-    const forestClassificationLambda = new lambda.Function(this, 'ForestClassificationLambda', {
+    const forestClassificationLambda = new lambda.Function(this, "ForestClassificationLambda", {
       runtime: lambda.Runtime.PYTHON_3_13,
-      handler: 'lambda_function.lambda_handler',
-      code: lambda.Code.fromBucket(assetsBucket, 'lambda-function-code.zip'),
+      handler: "lambda_function.lambda_handler",
+      code: lambda.Code.fromBucket(assetsBucket, "lambda-function-code.zip"),
       memorySize: 10240,
       timeout: cdk.Duration.seconds(900),
       environment: {
         S3_BUCKET: bucket.bucketName,
         ASSETS_BUCKET: assetsBucketName,
         EE_KEY_S3_KEY: geeCredentialsFile,
-        EE_KEY_PATH: '/tmp/ee-key.json',
-        DATA_PATH: '/tmp/data.json',
-        OUTPUT_PREFIX: 'forest_classification',
-        UPLOAD_EXPIRATION: '3600',
-        DOWNLOAD_EXPIRATION: '86400',
-        ALLOWED_ORIGINS: '*',
-        DEBUG: 'false',
+        EE_KEY_PATH: "/tmp/ee-key.json",
+        DATA_PATH: "/tmp/data.json",
+        OUTPUT_PREFIX: "forest_classification",
+        UPLOAD_EXPIRATION: "3600",
+        DOWNLOAD_EXPIRATION: "86400",
+        ALLOWED_ORIGINS: "*",
+        DEBUG: "false",
       },
       layers: [earthEngineLayer, imageProcessingLayer],
-    });
+    })
 
     const functionUrl = forestClassificationLambda.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE, // Public access; use AWS_IAM for IAM-based auth if needed
-    });
+    })
 
     // Grant the Lambda function permissions to read/write to the S3 bucket
-    bucket.grantReadWrite(forestClassificationLambda);
+    bucket.grantReadWrite(forestClassificationLambda)
 
     // Grant the Lambda function permissions to read from the assets bucket
-    assetsBucket.grantRead(forestClassificationLambda);
+    assetsBucket.grantRead(forestClassificationLambda)
 
     // Grant the Lambda function permissions to write logs
     forestClassificationLambda.addToRolePolicy(
       new iam.PolicyStatement({
-        actions: ['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+        actions: ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
         resources: [`arn:aws:logs:${this.region}:${this.account}:log-group:/aws/lambda/*:*`],
-      })
-    );
+      }),
+    )
+
+    // Frontend Build Bucket for Amplify deployment (similar to Catholic Charities)
+    const frontendBucket = new s3.Bucket(this, "FrontendBuildBucket", {
+      bucketName: `${bucketName}-builds-${this.account}-${this.region}`.substring(0, 63),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: false,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      eventBridgeEnabled: true,
+    })
+
+    // Grant Amplify service access to the build bucket
+    frontendBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        sid: "AllowAmplifyServiceAccess",
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.ServicePrincipal("amplify.amazonaws.com")],
+        actions: [
+          "s3:GetObject",
+          "s3:GetObjectAcl",
+          "s3:GetObjectVersion",
+          "s3:GetObjectVersionAcl",
+          "s3:PutObjectAcl",
+          "s3:PutObjectVersionAcl",
+          "s3:ListBucket",
+          "s3:GetBucketAcl",
+          "s3:GetBucketLocation",
+          "s3:GetBucketVersioning",
+          "s3:GetBucketPolicy",
+          "s3:GetBucketPolicyStatus",
+          "s3:GetBucketPublicAccessBlock",
+          "s3:GetEncryptionConfiguration",
+        ],
+        resources: [`${frontendBucket.bucketArn}/*`, frontendBucket.bucketArn],
+        conditions: {
+          StringEquals: {
+            "aws:SourceAccount": this.account,
+          },
+        },
+      }),
+    )
+
+    // Amplify Deployer Role
+    const amplifyDeployerRole = new iam.Role(this, "AmplifyDeployerRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")],
+      inlinePolicies: {
+        AmplifyAccess: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: [
+                "amplify:StartDeployment",
+                "amplify:GetApp",
+                "amplify:GetBranch",
+                "amplify:ListApps",
+                "amplify:ListBranches",
+                "amplify:GetJob",
+                "amplify:ListJobs",
+              ],
+              resources: ["*"],
+            }),
+          ],
+        }),
+        S3Access: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: [
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:GetObjectAcl",
+                "s3:GetObjectVersionAcl",
+                "s3:PutObjectAcl",
+                "s3:PutObjectVersionAcl",
+                "s3:ListBucket",
+                "s3:GetBucketAcl",
+                "s3:GetBucketLocation",
+                "s3:GetBucketVersioning",
+                "s3:PutBucketAcl",
+                "s3:ListBucketVersions",
+                "s3:GetBucketPolicy",
+                "s3:GetBucketPolicyStatus",
+                "s3:GetBucketPublicAccessBlock",
+                "s3:GetEncryptionConfiguration",
+              ],
+              resources: [frontendBucket.bucketArn, `${frontendBucket.bucketArn}/*`],
+            }),
+          ],
+        }),
+      },
+    })
+
+    // Amplify Deployer Lambda
+    const amplifyDeployer = new lambda.Function(this, "AmplifyDeployer", {
+      runtime: lambda.Runtime.PYTHON_3_13,
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+import boto3
+import json
+import logging
+import os
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+amplify_client = boto3.client('amplify')
+
+def handler(event, context):
+    try:
+        logger.info(f"Received EventBridge event: {json.dumps(event)}")
+        
+        if event.get('source') == 'aws.s3' and event.get('detail-type') == 'Object Created':
+            detail = event.get('detail', {})
+            bucket_name = detail.get('bucket', {}).get('name')
+            object_key = detail.get('object', {}).get('key')
+            
+            logger.info(f"Processing S3 object: {bucket_name}/{object_key}")
+            
+            if object_key and object_key.startswith('builds/') and object_key.endswith('.zip'):
+                app_id = os.environ.get('AMPLIFY_APP_ID')
+                
+                if not app_id:
+                    logger.error("AMPLIFY_APP_ID environment variable not set")
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps({'error': 'AMPLIFY_APP_ID not configured'})
+                    }
+                
+                branch_name = os.environ.get('AMPLIFY_BRANCH_NAME', 'main')
+                
+                logger.info(f"Starting Amplify deployment for app {app_id}, branch {branch_name}")
+                
+                response = amplify_client.start_deployment(
+                    appId=app_id,
+                    branchName=branch_name,
+                    sourceUrl=f"s3://{bucket_name}/{object_key}"
+                )
+                
+                job_id = response['jobSummary']['jobId']
+                logger.info(f"✅ Started Amplify deployment with job ID: {job_id}")
+                
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps({
+                        'message': 'Deployment started successfully',
+                        'jobId': job_id,
+                        'appId': app_id,
+                        'branchName': branch_name,
+                        'sourceUrl': f"s3://{bucket_name}/{object_key}"
+                    })
+                }
+            else:
+                logger.info(f"Skipping non-build file: {object_key}")
+        else:
+            logger.info(f"Skipping non-S3 event: {event.get('source')}")
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Event processed, no action needed'})
+        }
+        
+    except Exception as e:
+        logger.error(f"❌ Error: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
+`),
+      timeout: cdk.Duration.minutes(5),
+      role: amplifyDeployerRole,
+      environment: {
+        AMPLIFY_APP_ID: "placeholder",
+        AMPLIFY_BRANCH_NAME: "main",
+      },
+    })
+
+    // EventBridge rule to trigger Amplify deployment when build is uploaded
+    const s3UploadRule = new events.Rule(this, "S3BuildUploadRule", {
+      eventPattern: {
+        source: ["aws.s3"],
+        detailType: ["Object Created"],
+        detail: {
+          bucket: {
+            name: [frontendBucket.bucketName],
+          },
+          object: {
+            key: [{ prefix: "builds/" }, { suffix: ".zip" }],
+          },
+        },
+      },
+    })
+
+    s3UploadRule.addTarget(new targets.LambdaFunction(amplifyDeployer))
+
+    amplifyDeployer.addPermission("AllowEventBridgeInvoke", {
+      principal: new iam.ServicePrincipal("events.amazonaws.com"),
+      sourceArn: s3UploadRule.ruleArn,
+    })
 
     // Output the Lambda function ARN, S3 bucket name, and instructions
-    new cdk.CfnOutput(this, 'LambdaFunctionArn', {
+    new cdk.CfnOutput(this, "LambdaFunctionArn", {
       value: forestClassificationLambda.functionArn,
-      description: 'The ARN of the Forest Classification Lambda function',
-    });
+      description: "The ARN of the Forest Classification Lambda function",
+    })
 
-    new cdk.CfnOutput(this, 'S3BucketName', {
+    new cdk.CfnOutput(this, "S3BucketName", {
       value: bucket.bucketName,
-      description: 'The name of the S3 bucket for input/output files',
-    });
+      description: "The name of the S3 bucket for input/output files",
+    })
 
-    new cdk.CfnOutput(this, 'UploadInstruction', {
+    new cdk.CfnOutput(this, "UploadInstruction", {
       value: `Upload your data files to s3://${bucket.bucketName}/uploads/ and ensure the GEE credentials file is at s3://${bucket.bucketName}/${geeCredentialsFile}`,
-      description: 'Instructions for using the S3 bucket',
-    });
+      description: "Instructions for using the S3 bucket",
+    })
 
-    new cdk.CfnOutput(this, 'LambdaFunctionUrl', {
+    new cdk.CfnOutput(this, "LambdaFunctionUrl", {
       value: functionUrl.url,
-      description: 'The URL of the Forest Classification Lambda function for front-end integration',
-    });
-    
+      description: "The URL of the Forest Classification Lambda function for front-end integration",
+    })
+
+    new cdk.CfnOutput(this, "FrontendBuildBucketName", {
+      value: frontendBucket.bucketName,
+      description: "S3 Bucket for Frontend Build Artifacts",
+    })
+
+    new cdk.CfnOutput(this, "AmplifyDeployerFunctionName", {
+      value: amplifyDeployer.functionName,
+      description: "Amplify Deployer Lambda Function Name",
+    })
   }
 }
--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,46 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
-# Prompt for GitHub URL
 if [ -z "${GITHUB_URL:-}" ]; then
   read -rp "Enter source GitHub repository URL (e.g., https://github.com/OWNER/REPO): " GITHUB_URL
 fi
 
-# Normalize URL
 clean_url=${GITHUB_URL%.git}
 clean_url=${clean_url%/}
 
-# Extract owner/repo
-if [[ $clean_url =~ ^https://github\.com/([^/]+/[^/]+)$ ]]; then
-  path="${BASH_REMATCH[1]}"
-elif [[ $clean_url =~ ^git@github\.com:([^/]+/[^/]+)$ ]]; then
-  path="${BASH_REMATCH[1]}"
-else
-  echo "Unable to parse owner/repo from '$GITHUB_URL'"
-  read -rp "Enter GitHub owner: " GITHUB_OWNER
-  read -rp "Enter GitHub repo: " GITHUB_REPO
-fi
-
-if [ -z "${GITHUB_OWNER:-}" ] || [ -z "${GITHUB_REPO:-}" ]; then
-  GITHUB_OWNER=${path%%/*}
-  GITHUB_REPO=${path##*/}
-  echo "Detected GitHub Owner: $GITHUB_OWNER"
-  echo "Detected GitHub Repo: $GITHUB_REPO"
-  read -rp "Is this correct? (y/n): " CONFIRM
-  CONFIRM=$(printf '%s' "$CONFIRM" | tr '[:upper:]' '[:lower:]')
-  if [[ "$CONFIRM" != "y" && "$CONFIRM" != "yes" ]]; then
-    read -rp "Enter GitHub owner: " GITHUB_OWNER
-    read -rp "Enter GitHub repo: " GITHUB_REPO
-  fi
-fi
-
-# Prompt for deployment parameters
 if [ -z "${PROJECT_NAME:-}" ]; then
-  read -rp "Enter project name (e.g., OpenEarthProject): " PROJECT_NAME
+  read -rp "Enter project name [default: open-earth-project]: " PROJECT_NAME
+  PROJECT_NAME=${PROJECT_NAME:-open-earth-project}
 fi
 
 if [ -z "${BUCKET_NAME:-}" ]; then
-  read -rp "Enter S3 bucket name for data storage (must be globally unique): " BUCKET_NAME
+  read -rp "Enter S3 bucket name for data storage: " BUCKET_NAME
 fi
 
 if [ -z "${ASSETS_BUCKET:-}" ]; then
@@ -48,16 +22,36 @@ if [ -z "${ASSETS_BUCKET:-}" ]; then
 fi
 
 if [ -z "${GEE_CREDENTIALS_FILE:-}" ]; then
-  read -rp "Enter the path to GEE credentials file in assets bucket (e.g., credentials/gee-key.json): " GEE_CREDENTIALS_FILE
-fi
-# Verify project structure
-if [ ! -d "backend" ] || [ ! -d "Frontend" ]; then
-    echo "Error: Backend or Frontend directory not found!"
-    exit 1
+  read -rp "Enter GEE credentials file path [default: credentials/gee-key.json]: " GEE_CREDENTIALS_FILE
+  GEE_CREDENTIALS_FILE=${GEE_CREDENTIALS_FILE:-credentials/gee-key.json}
 fi
 
-# Create IAM role for CodeBuild
-ROLE_NAME="${PROJECT_NAME}-service-role"
+if [ -z "${AMPLIFY_APP_NAME:-}" ]; then
+  read -rp "Enter Amplify app name [default: ${PROJECT_NAME}-frontend]: " AMPLIFY_APP_NAME
+  AMPLIFY_APP_NAME=${AMPLIFY_APP_NAME:-${PROJECT_NAME}-frontend}
+fi
+
+if [ -z "${AMPLIFY_BRANCH_NAME:-}" ]; then
+  read -rp "Enter Amplify branch name [default: main]: " AMPLIFY_BRANCH_NAME
+  AMPLIFY_BRANCH_NAME=${AMPLIFY_BRANCH_NAME:-main}
+fi
+
+if [ -z "${AWS_REGION:-}" ]; then
+  read -rp "Enter AWS region [default: us-west-2]: " AWS_REGION
+  AWS_REGION=${AWS_REGION:-us-west-2}
+fi
+
+if [ -z "${ACTION:-}" ]; then
+  read -rp "Enter action [deploy/destroy]: " ACTION
+  ACTION=$(printf '%s' "$ACTION" | tr '[:upper:]' '[:lower:]')
+fi
+
+if [[ "$ACTION" != "deploy" && "$ACTION" != "destroy" ]]; then
+  echo "Invalid action: '$ACTION'. Choose 'deploy' or 'destroy'."
+  exit 1
+fi
+
+ROLE_NAME="${PROJECT_NAME}-codebuild-service-role"
 echo "Checking for IAM role: $ROLE_NAME"
 
 if aws iam get-role --role-name "$ROLE_NAME" >/dev/null 2>&1; then
@@ -79,72 +73,110 @@ else
     --assume-role-policy-document "$TRUST_DOC" \
     --query 'Role.Arn' --output text)
 
+  echo "Attaching policies..."
   aws iam attach-role-policy \
     --role-name "$ROLE_NAME" \
     --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
-
-  aws iam attach-role-policy \
-    --role-name "$ROLE_NAME" \
-    --policy-arn arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess
 
   echo "Waiting for IAM role to propagate..."
   sleep 10
 fi
 
-# Create main CodeBuild project
-echo "Creating CodeBuild project: ${PROJECT_NAME}"
+CODEBUILD_PROJECT_NAME="${PROJECT_NAME}-hybrid-deploy"
+echo "Creating CodeBuild project: $CODEBUILD_PROJECT_NAME"
 
-BACKEND_ENV='{
+ENV_VARS=$(cat <<EOF
+[
+  {"name": "PROJECT_NAME", "value": "$PROJECT_NAME", "type": "PLAINTEXT"},
+  {"name": "BUCKET_NAME", "value": "$BUCKET_NAME", "type": "PLAINTEXT"},
+  {"name": "ASSETS_BUCKET", "value": "$ASSETS_BUCKET", "type": "PLAINTEXT"},
+  {"name": "GEE_CREDENTIALS_FILE", "value": "$GEE_CREDENTIALS_FILE", "type": "PLAINTEXT"},
+  {"name": "ACTION", "value": "$ACTION", "type": "PLAINTEXT"},
+  {"name": "CDK_DEFAULT_REGION", "value": "$AWS_REGION", "type": "PLAINTEXT"},
+  {"name": "AMPLIFY_APP_NAME", "value": "$AMPLIFY_APP_NAME", "type": "PLAINTEXT"},
+  {"name": "AMPLIFY_BRANCH_NAME", "value": "$AMPLIFY_BRANCH_NAME", "type": "PLAINTEXT"}
+]
+EOF
+)
+
+ENVIRONMENT=$(cat <<EOF
+{
   "type": "LINUX_CONTAINER",
   "image": "aws/codebuild/standard:7.0",
-  "computeType": "BUILD_GENERAL1_SMALL",
-  "environmentVariables": [
-    {"name": "PROJECT_NAME", "value": "'"$PROJECT_NAME"'", "type": "PLAINTEXT"},
-    {"name": "BUCKET_NAME", "value": "'"$BUCKET_NAME"'", "type": "PLAINTEXT"},
-    {"name": "ASSETS_BUCKET", "value": "'"$ASSETS_BUCKET"'", "type": "PLAINTEXT"},
-    {"name": "GEE_CREDENTIALS_FILE", "value": "'"$GEE_CREDENTIALS_FILE"'", "type": "PLAINTEXT"}
-  ]
-}'
+  "computeType": "BUILD_GENERAL1_MEDIUM",
+  "environmentVariables": $ENV_VARS
+}
+EOF
+)
 
 ARTIFACTS='{"type":"NO_ARTIFACTS"}'
-SOURCE='{
+SOURCE=$(cat <<EOF
+{
   "type":"GITHUB",
-  "location":"'"$GITHUB_URL"'",
+  "location":"$GITHUB_URL",
   "buildspec":"buildspec.yml"
-}'
+}
+EOF
+)
+
+if aws codebuild batch-get-projects --names "$CODEBUILD_PROJECT_NAME" --query 'projects[0].name' --output text 2>/dev/null | grep -q "$CODEBUILD_PROJECT_NAME"; then
+  echo "Deleting existing CodeBuild project..."
+  aws codebuild delete-project --name "$CODEBUILD_PROJECT_NAME"
+  sleep 5
+fi
 
 aws codebuild create-project \
-  --name "$PROJECT_NAME" \
+  --name "$CODEBUILD_PROJECT_NAME" \
   --source "$SOURCE" \
-  --artifacts  "$ARTIFACTS" \
-  --environment "$BACKEND_ENV" \
+  --artifacts "$ARTIFACTS" \
+  --environment "$ENVIRONMENT" \
   --service-role "$ROLE_ARN" \
   --output json \
   --no-cli-pager
 
 if [ $? -eq 0 ]; then
-  echo "✓ CodeBuild project '$PROJECT_NAME' created."
+  echo "✓ CodeBuild project '$CODEBUILD_PROJECT_NAME' created."
 else
   echo "✗ Failed to create CodeBuild project."
   exit 1
 fi
 
-# Start build
-echo "Starting build for '$PROJECT_NAME'..."
-aws codebuild start-build \
-  --project-name "$PROJECT_NAME" \
-  --no-cli-pager \
-  --output json
+echo "Starting hybrid deployment..."
+BUILD_ID=$(aws codebuild start-build \
+  --project-name "$CODEBUILD_PROJECT_NAME" \
+  --query 'build.id' \
+  --output text)
 
 if [ $? -eq 0 ]; then
-  echo "✓ Build started."
+  echo "✓ Build started with ID: $BUILD_ID"
+  echo "You can monitor the build progress in the AWS Console:"
+  echo "https://console.aws.amazon.com/codesuite/codebuild/projects/$CODEBUILD_PROJECT_NAME/build/$BUILD_ID"
 else
   echo "✗ Failed to start build."
   exit 1
 fi
 
-echo "Current CodeBuild projects:"
-aws codebuild list-projects --output table
+echo ""
+echo "=== Open Earth Hybrid Deployment Information ==="
+echo "Project Name: $PROJECT_NAME"
+echo "GitHub Repo URL: $GITHUB_URL"
+echo "Amplify App Name: $AMPLIFY_APP_NAME"
+echo "Amplify Branch Name: $AMPLIFY_BRANCH_NAME"
+echo "Deployment Strategy: HYBRID"
+echo "  - Backend: CloudFormation (CDK)"
+echo "  - Amplify App: CLI (buildspec)"
+echo "  - Deployment: Automated (EventBridge + Lambda)"
+echo "Action: $ACTION"
+echo "Build ID: $BUILD_ID"
+echo ""
+echo "🚀 The hybrid deployment will:"
+echo "1. Deploy backend via CloudFormation"
+echo "2. Create/update Amplify app via CLI with name '$AMPLIFY_APP_NAME' and branch '$AMPLIFY_BRANCH_NAME'"
+echo "3. Build and upload frontend to S3"
+echo "4. Automatically deploy via EventBridge trigger"
+echo "5. No manual steps required!"
+echo ""
+echo "⏱️ Total deployment time: ~10-15 minutes"
+echo "📊 Monitor progress in CodeBuild console above"
 
 exit 0
-
